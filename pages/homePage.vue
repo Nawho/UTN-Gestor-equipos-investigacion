d47b146c873bc1824ea81be124cf46ce59fcbda8
Margatronix <alumno23.margareto.agustin@ipm.edu.ar>
2023-08-08 14:37:44 -0300

    letooo

---
+++ pages/homePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>homeeee</h2>
+        <br>
+        <button class="btn btn-primary" @click="$router.push('/projects/adProjects')">gestion de proyectos</button>
+    </div>
+    <br>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
