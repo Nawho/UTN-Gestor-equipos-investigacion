ccf27ced3fe07c734cb755bdc830228c2bc7cb27
tole <leto@example.com>
2023-10-31 17:45:18 -0300

    Integración con la api del sector de actividades

--- pages/equipo/[equipo]/activities/index.vue
+++ pages/equipo/[equipo]/activities/index.vue
@@ -19,13 +19,13 @@
         </div>
     </div>
     <div class="flex flex-wrap justify-center">
-        <div class="p-8" v-for="n in 6">
+        <div class="p-8" v-for="n in projectos">
             <div class="card w-96 border-4 border-indigo-500/75">
                 <img class="rounded-t-xl" src="/imgs/actividades.png" alt="Shoes" />
                 <div class="card-body">
-                    <h2 class="card-title">Actividad epicaa</h2>
-                    <div class="badge badge-outline">Presencial</div>
-                    <p>queres ser tu propio jefe ???</p>
+                    <h2 class="card-title">{{n.nombre}}</h2>
+                    <div class="badge badge-outline">{{ n.asistencia }}</div>
+                    <p>{{ n.descripcionCorta }}</p>
                     <div class="card-actions justify-end">
                         <button class="btn btn-primary">edit</button>
                     </div>
@@ -40,7 +40,8 @@ import { error } from 'console';
 const {equipo} = useRoute().params;
 interface project{
     nombre:string,
-    descripcion:string,
+    descripcionCorta:string,
+    asistencia:string,
     fechaDeInicio:string,
     fechaFinal:string
 
@@ -50,15 +51,17 @@ interface project{
 const a = await $fetch('http://localhost:3009/grupo_investigacion').catch((err) => {
     console.error(err.data)
 })
-
-let projectos:project[] = []
+let projectsID:string[]=[]
+const projectos = ref([])
 
 a.forEach(x => {
     console.log(x.nombre);
     if(x.nombre == equipo ){ 
-        console.log(x._id);
-        x.proyectos.forEach(y => {
-            projectos.push(y);
+        console.log(x);
+        x.actividades.forEach(y => {
+            console.log(y.activityid);
+            
+            projectsID.push(y.activityid);
         });
         
      }
@@ -67,6 +70,13 @@ a.forEach(x => {
      
     
 });;
+projectsID.forEach( async (x)=>{
+
+    let activity =await $fetch('http://localhost:3009/actividad/'+x) as project 
+    console.log(activity);
+    
+    projectos.value.push( activity )
+} )
 
 </script>
 
